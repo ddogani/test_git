5c07a23967d3f20127eb2d0080a859c8e0208fa4
Deokhwan Jung <ddogani@gmail.com>
2011-10-10 10:31:19 +0900

    add 2 files

---
+++ test2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+	int b = 0;
+
+	printf("dfadlkjfdkfjljkjlkj\n");
+
+	return 0;
+
+}
---
+++ test3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(){
+	int c = 1;
+
+	printf("%d dfdfadf\n", c);
+
+
+	return 0;
+
+}
